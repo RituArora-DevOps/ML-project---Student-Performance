8309b65893a4bd53b21f490132a7802232f876fc
Ritu Arora <jsr.ritu.jsr@gmail.com>
2025-09-23 16:17:04 -0400

    model training with hyperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,14 +7,15 @@ from src.exception import CustomException
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 from src.utils import save_object
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artrifacts', 'train.csv')
-    test_data_path: str=os.path.join('artrifacts', 'test.csv')
-    raw_data_path: str=os.path.join('artrifacts', 'data.csv')
+    train_data_path: str=os.path.join('artifacts', 'train.csv')
+    test_data_path: str=os.path.join('artifacts', 'test.csv')
+    raw_data_path: str=os.path.join('artifacts', 'data.csv')
 
 class DataIngestion:
     def __init__(self):
@@ -51,4 +52,7 @@ if __name__ == '__main__':
     train_data, test_data, _ = obj.initiate_data_ingestion() 
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
